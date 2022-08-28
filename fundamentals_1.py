f27c9de47767cb330886b5fd6fd1106a06f07499
Ananda Narayanan Udayakumar <ananduun@icloud.com>
2022-08-28 11:31:08 -0400

    some codes added

--- fundamentals_1.py
+++ fundamentals_1.py
@@ -1,1 +1,12 @@
 #here we do fundamental exercises for python
+
+#1. Write a program that asks the user for their name and then prints a greeting to the user.
+# Write your code here
+name = input("What is your name? ")
+print("Hello " + name)
+
+#2. Write a program that asks the user for a number n and gives the square of n.
+# Write your code here
+number = int(input("Give me a number: "))
+print("Your number is: ", number)
+print("Your square is: ", number ** 2)
